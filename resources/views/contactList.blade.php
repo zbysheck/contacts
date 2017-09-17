ad5e1b21b0bb7954cded06855a139664357d8d39
Zbyszek Kisły <zbyshekh@gmail.com>
2017-09-17 19:38:09 +0200

    nicer list

--- resources/views/contactList.blade.php
+++ resources/views/contactList.blade.php
@@ -1,10 +1,14 @@
+@extends("layouts/material")
 
+@section("content")
+<ul class="demo-list-icon mdl-list">
 @foreach($contacts as $contact)
-
-
-
-    <a href="{{route('singleContact', $contact->id)}}">{{$contact->name}} {{$contact->surname}}</a><br/>
-
-
-
+    <li class="mdl-list__item">
+        <span class="mdl-list__item-primary-content">
+        <i class="material-icons mdl-list__item-icon">person</i>
+        <a href="{{route('singleContact', $contact->id)}}">{{$contact->name}} {{$contact->surname}}</a><br/>
+    </span>
+    </li>
 @endforeach
+</ul>
+@endsection
