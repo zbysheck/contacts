ec032913087a6ea31c7ea4afa610f8226fce47dd
Zbyszek Kisły <zbyshekh@gmail.com>
2017-09-17 22:56:23 +0200

    nic single and authentication

--- resources/views/singleContact.blade.php
+++ resources/views/singleContact.blade.php
@@ -1,9 +1,36 @@
-{{$contact->name}}
-{{$contact->surname}}
-{{$contact->phone}}
-{{$contact->mail}}
-{{$contact->birth}}
+@extends("layouts/material")
+
+@section("content")
+
+    <ul class="demo-list-icon mdl-list">
+        <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon">person</i>
+                {{$contact->name}}
+                {{$contact->surname}}
+            </span>
+        </li>
+        <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon">phone</i>
+                {{$contact->phone}}
+            </span>
+        </li>
+        <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon">mail</i>
+                {{$contact->mail}}
+            </span>
+        </li>
+        <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon">event</i>
+                {{$contact->birth}}
+            </span>
+        </li>
 @if(Auth::user())
 <a href="{{route('contactEdit', $contact->id)}}">edit user</a><br/>
 
-    @endif
+@endif
+    </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/list', 'Contact@showList')->name('contactList');
 
 Route::get('/contact/{id}', 'Contact@single')->name('singleContact');
-Route::get('/edit/{id}', 'Contact@edit')->name('contactEdit');
-Route::post('/edit', 'Contact@editPost')->name('contactEditPost');
-Route::get('/new', 'Contact@add')->name('contactNew');
-Route::post('/new', 'Contact@addPost')->name('contactNewPost');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/edit/{id}', 'Contact@edit')->name('contactEdit');
+    Route::post('/edit', 'Contact@editPost')->name('contactEditPost');
+    Route::get('/new', 'Contact@add')->name('contactNew')->middleware('auth');
+    Route::post('/new', 'Contact@addPost')->name('contactNewPost');
 
-Route::get("/exists/{mail}", function ($mail){
-    return \App\Contact::where("mail", "=", $mail)->count();
+    Route::get("/exists/{mail}", function ($mail) {
+        return \App\Contact::where("mail", "=", $mail)->count();
+    });
 });
